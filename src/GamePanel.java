39b2c1ccae614e79aee8907d220ede60993660b1
sivavarunan <thevarasa.20221591@iit.ac.lk>
2024-10-27 16:08:36 +0530

    variables defined

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,9 +1,26 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.util.Random;
 import java.util.random.*;
 
 public class GamePanel extends JPanel implements ActionListener {
+
+    static final int SCREEN_WIDTH = 600;
+    static final int SCREEN_HEIGHT = 600;
+    static final int UNIT_SIZE = 25;
+    static final int GAME_UNITS = (SCREEN_WIDTH*SCREEN_HEIGHT)/UNIT_SIZE;
+    static final int DELAY = 75;
+    final int x[] = new int[GAME_UNITS];
+    final int y[] = new int[GAME_UNITS];
+    int bodyParts = 6;
+    int applesEaten;
+    int appleX;
+    int appleY;
+    char direction= 'R';
+    boolean running = false;
+    Timer timer;
+    Random random;
 
     GamePanel(){
 
